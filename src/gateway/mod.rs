190fb1aa00e754cabffce58309524c3517c618fa
chavic <dev.chavic@gmail.com>
2024-01-25 14:56:57 +0200

    cleaned out api

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -3,77 +3,25 @@ use async_graphql::*;
 use actix_web::{web, HttpResponse};
 use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
 
-use crate::remote::services::*;
 use models::*;
 
 mod models;
 mod error;
 
 pub type MySchema = Schema<QueryRoot, MutationRoot, SubscriptionRoot>;
-pub type MySchema = Schema<Query, Mutation, Subscription>;
 
 pub struct MutationRoot;
 
 #[Object]
 impl MutationRoot {
-    // Creates a new user account
-    async fn create_user_account(&self, ctx: &Context<'_>, input: NewUserAccount) -> Result<UserAccount> {
-        // Logic to create a new user account
-        todo!()
-    }
-
-    // Updates an existing user account
-    async fn update_user_account(&self, ctx: &Context<'_>, input: UpdateUserAccount) -> Result<UserAccount> {
-        // Logic to update a user account
-        todo!()
-    }
-
-    // Deletes a user account
-    async fn delete_user_account(&self, ctx: &Context<'_>, user_id: ID) -> Result<bool> {
-        // Logic to delete a user account
-        todo!()
-    }
-
-    // Authenticates a user
-    async fn authenticate_user(&self, ctx: &Context<'_>, input: AuthenticationInput) -> Result<AuthenticationPayload> {
-        // Logic to authenticate a user
-        todo!()
-    }
-
-    // Changes a user's password
-    async fn change_user_password(&self, ctx: &Context<'_>, input: ChangePasswordInput) -> Result<bool> {
-        // Logic to change a user's password
-        todo!()
-    }
-
-    // Sets a user's permissions
-    async fn set_user_permissions(&self, ctx: &Context<'_>, input: SetPermissionsInput) -> Result<bool> {
-        // Logic to set a user's permissions
-        todo!()
-    }
-
-    // Define mutation methods here
-    // Example: Create a new loan application
-    async fn create_loan_application(&self, ctx: &Context<'_>, application: LoanApplicationInput) -> Result<LoanApplication> {
-        // Logic to create a new loan application
-        todo!()
-    }
-
-    // Add more mutation methods here for each service
+    
 }
 
 pub struct SubscriptionRoot;
 
 #[Subscription]
 impl SubscriptionRoot {
-    // Define subscription methods here
-    // Example: Subscribe to loan application updates
-    async fn loan_application_updates(&self, ctx: &Context<'_>, application_id: ID) -> impl Stream<Item = Result<LoanApplication>> {
-        // Logic to stream updates for a loan application
-        todo!()
-    }
-
-    // Add more subscription methods here for each service
+    
 }
 
 pub struct QueryRoot;
@@ -84,50 +32,10 @@ impl QueryRoot {
         "0.1"
     }
 
-    // User Management Service
-    async fn user_account(&self, user_id: ID) -> Result<UserAccount> {
-        // Fetch user account logic here
-        todo!()
-    }
-
-    // Financial Transactions Service
-    async fn transaction_history(&self, user_id: ID) -> Result<Vec<Transaction>> {
-        // Fetch transaction history logic here
-        todo!()
-    }
-
-    // Credit Scoring Service
-    async fn credit_score(&self, user_id: ID) -> Result<CreditScore> {
-        // Calculate credit score logic here
-        todo!()
-    }
-
-    // Lending Service
-    async fn loan_application(&self, application_id: ID) -> Result<LoanApplication> {
-        // Fetch loan application logic here
-        todo!()
-    }
-
-    // Card Integration Service
-    async fn card_details(&self, user_id: ID) -> Result<CardDetails> {
-        // Fetch card details logic here
-        todo!()
-    }
-
-    // Point of Sale Service
-    async fn sale_transaction(&self, transaction_id: ID) -> Result<SaleTransaction> {
-        // Fetch sale transaction logic here
-        todo!()
-    }
-
-    // Add more query methods here for each service
-    // Add more methods here for each field in the Query type in the GraphQL schema.
-    // Each method should return a Result and use the models to fetch the data.
 }
 
 pub fn schema() -> MySchema {
     Schema::build(QueryRoot, MutationRoot, SubscriptionRoot)
-    Schema::build(Query, Mutation, Subscription)
         .finish()
 }
 
